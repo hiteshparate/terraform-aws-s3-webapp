422f46b85d525a99458b99f509adc6ca44114e68
Hitesh Parate <48521519+hiteshparate@users.noreply.github.com>
2023-02-06 14:14:16 +0530

    added description in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
-variable "region" {}
-variable "prefix" {}
-variable "name" {}
+variable "region" {
+  description = "This is the region where cloud willl host resources"
+}
+variable "prefix" {
+  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
+}
+variable "name" {
+  description = "Your name to attach to the webapp address"
+}
