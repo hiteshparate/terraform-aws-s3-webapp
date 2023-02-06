b734f63ce3bc2d4bf8f0b3541443b5c406e578a4
Hitesh Parate <48521519+hiteshparate@users.noreply.github.com>
2023-02-06 14:20:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/self-learning/s3-webapp/aws"
+  source  = "https://app.terraform.io/app/sellf-learning/registry/modules/private/sellf-learning/s3-webapp/aws/1.0.0"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
